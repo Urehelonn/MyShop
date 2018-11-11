dd608b30223c7ddfeaf12741ebee2b6a7cd7c4ff
Liam <urehelonn@gmail.com>
2018-11-11 02:39:43 -0500

    product category filter works commit

---
+++ MyShop/MyShop.Core/Models/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { set; get; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -19,10 +19,23 @@ namespace MyShop.WebUI.Controllers
             productCategories = productCategoriesContext;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+            if (Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else{
+                // if there is category restriction, filter only prodcut with the required category
+                products = context.Collection().Where(p => p.Category==Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+            return View(model);
         }
 
         public ActionResult About()
